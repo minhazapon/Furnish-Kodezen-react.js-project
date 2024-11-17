431a2f3a8fcb24013c96a403953cb879bd2f4914
minhazapon <minhajulabedinapon@gmail.com>
2024-11-17 05:37:29 -0800

    added nav file

---
+++ src/Nav.jsx
@@ -0,0 +1,37 @@
+
+
+
+const Nav = () => {
+    return (
+        <div>
+
+
+            <div className=" navbar ">
+                
+                <div>
+                  
+                <p className="navName  ">FURNISH</p>
+
+                </div>
+               
+
+
+                 
+                <div>
+
+                    <button className=" navAbout  ">About us</button>
+                    <button className=" navAbout  ">Contact us</button>
+                    <button className=" navSign ">Sign In</button>
+                    
+                    
+                </div> 
+
+
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Nav;
