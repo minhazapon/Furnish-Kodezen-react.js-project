354ddfa202476dd562561417249fcdd4de943b99
minhazapon <minhajulabedinapon@gmail.com>
2024-11-20 07:41:32 -0800

    added file

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -8,25 +8,22 @@ const Nav = () => {
 
     return (
         <div>
-            <div className=" navbar ">
-                <div className=" navLogo  ">
-                <NavLink to='/'>
-                <img className=" " src="https://cdn-icons-png.flaticon.com/128/5234/5234853.png" alt="" height="50px" width="60px" /> 
-                </NavLink>
-                <p className="navName   ">FURNISH</p>
-                </div>
-                <div className=" navAllButton ">
-                <NavLink to='/AboutMother'><button className=" navAbout buttonHover">About us</button></NavLink>
-                <NavLink to='/contact' ><button className=" navAbout buttonHover">Contact us</button></NavLink>
-                <button className=" navSign  buttonHover ">Sign In</button>
-                </div> 
+        <div className=" navbar  " >
 
-               
-                
+        <div className=" navLogo  ">
+        <NavLink to='/'>
+        <img className=" " src="https://cdn-icons-png.flaticon.com/128/5234/5234853.png" alt="" height="50px" width="60px" /> 
+        </NavLink>
+        <p className="navName   ">FURNISH</p>
+        </div>
+        <div className=" navAllButton " >
+        <NavLink to='/AboutMother'><button className=" navAbout buttonHover">About</button></NavLink>
+        <NavLink to='/contact' ><button className=" navAbout buttonHover">Contact</button></NavLink>
+        <button className=" navSign  buttonHover ">Login</button>
+        </div> 
 
 
-
-            </div>
+        </div>
         </div>
     );
 };
