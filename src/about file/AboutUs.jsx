f1d33d56a26adb9ac8b03532a346f972f022e9cd
minhazapon <minhajulabedinapon@gmail.com>
2024-11-20 01:15:57 -0800

    added file

--- src/about file/AboutUs.jsx
+++ src/about file/AboutUs.jsx
@@ -8,6 +8,7 @@ const AboutUs = () => {
         <div className=" AboutMain ">
 
         <div>
+        <div className=" a-pic-text ">
         <div className=" about-header-text ">
         <p className=" aboutText  C1 ">About Us</p>
         <p className=" aboutText C2  ">About Furnish</p>
@@ -15,24 +16,30 @@ const AboutUs = () => {
         </div>
         <div className=" about-pic ">
         <img className=" pic-border  " width="100%" height="600px" src="https://s3-alpha-sig.figma.com/img/7626/6c57/332d776b1554bd87a49bc25cc505b08c?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iW2tB5E~Lc2qUCTCW~RkG1BZqGh9rn8fiork1exOdndTvzMMVCNHJ8vtpDH9K7x266Diu7BWdpt0~PZQzAn-Snl3wBloPu~rGZu5TT1uhzXkCkcB5cmfQHEG-Wb-89PxILqip0wlVaSrwrRUKjMI1CiAl4Pcaep3OnYH1-6t7o2fs1fJAHzU6ohROI01nCdFXHcpg72MkKwUhiwQgl3gHRqLXSbbCukZ9EGXxKLe98EFjk4HeOuC0JZsmdKXX1UpgZNiFTflKvV7mxonkEqgP9kCnd9JVP42mkTR-P5GDs-70sws-nbvEK34sDDH5EjgdY3sEa52ERGo0ucdaXLYKQ__" alt="" />
-        </div>
+        </div>  
+        </div>   
         <div className=" NumberContainer ">
         <div>
         <p className=" numText ">10k+</p>
         <p className=" num2text ">Products Sold</p>
         </div>
+
         <div>
         <p className=" numText ">35+</p>
         <p className=" num2text ">Years Service</p>
         </div>
+
         <div>
         <p className=" numText ">90+</p>
         <p className=" num2text ">Outlets Worldwide</p>
         </div>
+
         <div>
         <p className=" numText ">1000+</p>
         <p className=" num2text ">Satisfied Customers</p>
         </div>
+
+
         </div>
         </div>
 
