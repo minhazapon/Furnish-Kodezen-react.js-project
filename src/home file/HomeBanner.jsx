dbb6acfbd2801bf7ef1d4e1f70333726dd5f9fd1
minhazapon <minhajulabedinapon@gmail.com>
2024-11-25 06:01:17 -0800

    added file

--- src/home file/HomeBanner.jsx
+++ src/home file/HomeBanner.jsx
@@ -8,11 +8,11 @@ const HomeBanner = () => {
     return (
         <div className=" bannerAll ">
             <div className="  all-banner-text ">
-               <p className=" bannerText ExText   ">Explore any kind of furnitures</p>
+               <p className=" bannerText ExText   ">EXPLORE ANY KIND OF FURNITURES</p>
                <p className="   bannerText DiscoverText ">Discover Your<br></br>
                 Dream Furniture</p>
                <div className=" BannerB2 ">
-               <button className="   bannerButton buttonHover ">Check Products</button>
+               <button className="   bannerButton buttonHover inter ">Check Our Products</button>
                </div>
             </div>
         </div>
