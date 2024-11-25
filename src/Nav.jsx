933884d72b1e55091795375533d7714c4947e1c1
minhazapon <minhajulabedinapon@gmail.com>
2024-11-25 05:30:13 -0800

    added file

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -10,17 +10,19 @@ const Nav = () => {
 
       
 
-        <div className=" navbar" >    
-        <div className=" navLogo">
+        <div className=" navbar  " >     
+        <div className=" navLogo nav-margin-left ">
         <NavLink to='/'>
         <img className=" " src="https://cdn-icons-png.flaticon.com/128/5234/5234853.png" alt="" height="40px" width="50px" /> 
         </NavLink>
         <p className="navName   ">FURNISH</p>
         </div>
-        <div className=" navAllButton " >
-        <NavLink to='/AboutMother'><button className=" navAbout buttonHover">About</button></NavLink>
-        <NavLink to='/contact' ><button className=" navAbout buttonHover">Contact</button></NavLink>
-        <button className=" navSign  buttonHover ">Login</button>
+        <div className=" navAllButton nav-margin-right " >
+        <NavLink to='/AboutMother'><button className=" navAbout font-dm-sans buttonHover">About us</button></NavLink>
+        <NavLink to='/contact' ><button className=" navAbout font-dm-sans buttonHover">Contact Us</button></NavLink>
+        <div className=" text-sign ">
+        <button className=" navSign dm-sans  buttonHover ">Sign In</button>
+        </div>
         </div> 
         </div>
 
--- src/home file/HomeBanner.jsx
+++ src/home file/HomeBanner.jsx
@@ -10,7 +10,7 @@ const HomeBanner = () => {
             <div className="  all-banner-text ">
                <p className=" bannerText ExText   ">Explore any kind of furnitures</p>
                <p className="   bannerText DiscoverText ">Discover Your<br></br>
-               Dream Furniture</p>
+                Dream Furniture</p>
                <div className=" BannerB2 ">
                <button className="   bannerButton buttonHover ">Check Products</button>
                </div>
