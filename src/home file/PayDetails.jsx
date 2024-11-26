f8c2bf966d1943a22934c3fdefdd3712885f6201
minhazapon <minhajulabedinapon@gmail.com>
2024-11-25 21:31:19 -0800

    added file

--- src/home file/PayDetails.jsx
+++ src/home file/PayDetails.jsx
@@ -14,7 +14,7 @@ const PayDetails = () => {
                  <p className=" lorem ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit et nisl<br></br> tristique nulla. Nisl, egestas molestie vestibulum lectus habitasse donec morbi.<br></br> At est parturient id eu ut. </p>
                  <div className=" CV ">
                  <button className=" CU-b buttonHover " >Contact Us</button>
-                 <Link><p className=" VP">View Projects</p></Link>
+                 <a href="" className=" VP">View Projects</a>
                  </div>
                 </div>
             </div>
