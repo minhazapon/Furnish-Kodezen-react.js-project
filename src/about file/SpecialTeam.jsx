4facbf78d1af62d945f8c2c9dbcf58022a2cb82c
minhazapon <minhajulabedinapon@gmail.com>
2024-11-26 04:45:04 -0800

    added file

--- src/about file/SpecialTeam.jsx
+++ src/about file/SpecialTeam.jsx
@@ -16,24 +16,24 @@ const SpecialTeam = () => {
 
             <div className=" all-special-team-card ">
             <div>
-            <img className="hv1" height="250px" width="300px" src="https://s3-alpha-sig.figma.com/img/8c1d/bf7a/ed73796c1c98e327ebf3ef5a3cd0544f?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l91cIMTUtVMfuvkW8ABJmnpFymyXSibVLhTCHt~jff7SfCqBtTo1etYzHIiBeRDc6jqFqxmRvCQYir2hZjgG-RCsM05ZDIpOnr~dkA9~suCeaZV8Two6dkepG2AgykDNHJRCv7J-732ZtbxdpiYcSMqrfBlOJcvQ9VTlMQl~oHhTO69S-EB9cyStozkQ4u42s1avnPFQEPgRtsky5QDSXXqBDHj5DzDpTiASJQDw46YgRsqFFbmM588GXC-uaxbCtWo-XGV9XBRD~dMkogw9yv4rFy7vppil~t~VJmYNfnDsNwfxKERYIA-yOJR2-5UeGN4nzJDlg40fnZ~caX1yFQ__" alt="" />
+            <img className="hv1" height="250px" width="275px" src="https://s3-alpha-sig.figma.com/img/8c1d/bf7a/ed73796c1c98e327ebf3ef5a3cd0544f?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l91cIMTUtVMfuvkW8ABJmnpFymyXSibVLhTCHt~jff7SfCqBtTo1etYzHIiBeRDc6jqFqxmRvCQYir2hZjgG-RCsM05ZDIpOnr~dkA9~suCeaZV8Two6dkepG2AgykDNHJRCv7J-732ZtbxdpiYcSMqrfBlOJcvQ9VTlMQl~oHhTO69S-EB9cyStozkQ4u42s1avnPFQEPgRtsky5QDSXXqBDHj5DzDpTiASJQDw46YgRsqFFbmM588GXC-uaxbCtWo-XGV9XBRD~dMkogw9yv4rFy7vppil~t~VJmYNfnDsNwfxKERYIA-yOJR2-5UeGN4nzJDlg40fnZ~caX1yFQ__" alt="" />
             <p className=" olivia-name ">Olivia Rhye</p>
-            <p className=" olivia-name FC-color ">Founder & CEO</p>
+            <p className=" olivia-name1 FC-color ">Founder & CEO</p>
             </div>
             <div>
-            <img className="hv1" height="250px" width="300px" src="https://s3-alpha-sig.figma.com/img/261d/783d/a4147da7fea569d1940840845897657f?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lk8wFMkYW52hGkKcp1rZTPG39Yq3mw42dvq4WLVRSys2v0J64v6VbDVDK2rJ1p4-R0pw2JCiHYDG3YLVi5GuGB9u3XmhDKQwWwbqtg-DIjI6JDn2ZpVik9MV-xjSSNDoCdBQubt2~lmDDdp6IRyblcrUtqWW-5js7nab8cQtFsLCj2QGy~89YRm6p4X7C3XmV9Gm6LOO8kkBtdiWkl5u230UmtbxL3gA1omTulIeZR5eGLEnrfD3Lrlxw0osxi5LluqoDe1eaYYmAyJfkHnLmOPRX70XcKNeeTOXezHcsNr-mrGONAsT4dodol-MXGniEO-J307EvTR5M7M3SPTsag__" alt="" />
+            <img className="hv1" height="250px" width="275px" src="https://s3-alpha-sig.figma.com/img/261d/783d/a4147da7fea569d1940840845897657f?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lk8wFMkYW52hGkKcp1rZTPG39Yq3mw42dvq4WLVRSys2v0J64v6VbDVDK2rJ1p4-R0pw2JCiHYDG3YLVi5GuGB9u3XmhDKQwWwbqtg-DIjI6JDn2ZpVik9MV-xjSSNDoCdBQubt2~lmDDdp6IRyblcrUtqWW-5js7nab8cQtFsLCj2QGy~89YRm6p4X7C3XmV9Gm6LOO8kkBtdiWkl5u230UmtbxL3gA1omTulIeZR5eGLEnrfD3Lrlxw0osxi5LluqoDe1eaYYmAyJfkHnLmOPRX70XcKNeeTOXezHcsNr-mrGONAsT4dodol-MXGniEO-J307EvTR5M7M3SPTsag__" alt="" />
             <p className=" olivia-name ">Phoenix Baker</p>
-            <p className=" olivia-name FC-color ">Founder & CEO</p>
+            <p className=" olivia-name1 FC-color ">Product Designer</p>
             </div>
             <div>
-            <img className="hv1" height="250px" width="300px" src="https://s3-alpha-sig.figma.com/img/e71d/725e/7e94ea2c366e9caa5427cc55e4be1d29?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l-qskLooa3Lm-D8vkmbJm9Obp3~-75VCQZWcka93MffE7-wrDXstSbJaACXtSuqKGpMbLwHvRBb2kcLyl8oX4ys5iVSBq7oH9qNljsTARV5WXxTnJxT~5LgE0lNnJY0J6JoJ-Iex0QZh-bTNlzPwsQ2eNCQRbjnEVeG9pIDxP4plI-lfPHiMijOEVwb0L6kqLxohLOe0oGA2qzUIi45RPZ~WQMFiXnKqmerHN1VAQlh5B8YR0PWpx6MtaPJM9SL3~A6n7lD~6yZELfpw1-PRn~kZn6nN5PJWgctgq4zOTNzvEkJxvZGpkkfG9VGV3UBHI2baTG88FuA0NbJyhFttqg__" alt="" />
+            <img className="hv1" height="250px" width="275px" src="https://s3-alpha-sig.figma.com/img/e71d/725e/7e94ea2c366e9caa5427cc55e4be1d29?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=l-qskLooa3Lm-D8vkmbJm9Obp3~-75VCQZWcka93MffE7-wrDXstSbJaACXtSuqKGpMbLwHvRBb2kcLyl8oX4ys5iVSBq7oH9qNljsTARV5WXxTnJxT~5LgE0lNnJY0J6JoJ-Iex0QZh-bTNlzPwsQ2eNCQRbjnEVeG9pIDxP4plI-lfPHiMijOEVwb0L6kqLxohLOe0oGA2qzUIi45RPZ~WQMFiXnKqmerHN1VAQlh5B8YR0PWpx6MtaPJM9SL3~A6n7lD~6yZELfpw1-PRn~kZn6nN5PJWgctgq4zOTNzvEkJxvZGpkkfG9VGV3UBHI2baTG88FuA0NbJyhFttqg__" alt="" />
             <p className=" olivia-name ">Demi Wilkinson</p>
-            <p className=" olivia-name FC-color ">Managing Director</p>
+            <p className=" olivia-name1 FC-color ">Managing Director</p>
             </div>
             <div>
-            <img className="hv1" height="250px" width="300px" src="https://s3-alpha-sig.figma.com/img/0087/a98f/d098bd212153d72e003fd557d144df8b?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=K8VmYj~~dPOBITlmcHQoYAtuNoUP5dOa2NBZuzbTFuIxPD1DSLssUYlTfG5LjCmWnsgxY8Z-RRDTuvKm592TjT~fG9ALkNJIF8eyG1HxXSTgf11PebMeLI~YmWdyfncWhEQ8u8lDXkL-9uS3vqIHSv04fbsVDltkz77jl8866AsRNjTyIUqgcnOTUEQoys4ddyWorxXwGJACLlmfHerbCsilFbrvhk3f0sZcevoupHKIGQ7MkB4IAjpQWzRwIxazPAt4e4AoYy548LdWDqWLKTXnajJWSs~MtTdqVqUgWwVI5P5U4lVS58xnAolj54Dz7-F5jp6HuY9HeJ1dkJpr2w__" alt="" />
+            <img className="hv1" height="250px" width="275px" src="https://s3-alpha-sig.figma.com/img/0087/a98f/d098bd212153d72e003fd557d144df8b?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=K8VmYj~~dPOBITlmcHQoYAtuNoUP5dOa2NBZuzbTFuIxPD1DSLssUYlTfG5LjCmWnsgxY8Z-RRDTuvKm592TjT~fG9ALkNJIF8eyG1HxXSTgf11PebMeLI~YmWdyfncWhEQ8u8lDXkL-9uS3vqIHSv04fbsVDltkz77jl8866AsRNjTyIUqgcnOTUEQoys4ddyWorxXwGJACLlmfHerbCsilFbrvhk3f0sZcevoupHKIGQ7MkB4IAjpQWzRwIxazPAt4e4AoYy548LdWDqWLKTXnajJWSs~MtTdqVqUgWwVI5P5U4lVS58xnAolj54Dz7-F5jp6HuY9HeJ1dkJpr2w__" alt="" />
             <p className=" olivia-name ">Demi Wilkinson</p>
-            <p className=" olivia-name FC-color ">VP Sales</p>
+            <p className=" olivia-name1 FC-color ">VP Sales</p>
             </div>
             </div>
 
