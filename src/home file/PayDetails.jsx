5f7167496be34480edf70272b06b6c350f66a7a6
minhazapon <minhajulabedinapon@gmail.com>
2024-11-25 21:17:59 -0800

    added file

--- src/home file/PayDetails.jsx
+++ src/home file/PayDetails.jsx
@@ -8,7 +8,7 @@ const PayDetails = () => {
         <div>
 
             <div className=" Pay-img ">
-                <div>
+                <div className=" pay-margin ">
                  <p className=" pay-text  ">We pay details in<br></br> 
                  every our work</p>
                  <p className=" lorem ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit et nisl<br></br> tristique nulla. Nisl, egestas molestie vestibulum lectus habitasse donec morbi.<br></br> At est parturient id eu ut. </p>
