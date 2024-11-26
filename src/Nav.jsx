fd326b97372f6e0586d80589c15c0854e0413f3b
minhazapon <minhajulabedinapon@gmail.com>
2024-11-26 03:05:54 -0800

    added files

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -6,11 +6,11 @@ const Nav = () => {
 
 
     return (
-        <div className="  ">
+        <div className="  FixedNav ">
 
       
 
-        <div className=" navbar  " >     
+        <div className=" navbar   " >     
         <div className=" navLogo nav-margin-left ">
         <NavLink to='/'>
         <img className=" " src="https://cdn-icons-png.flaticon.com/128/5234/5234853.png" alt="" height="40px" width="50px" /> 
