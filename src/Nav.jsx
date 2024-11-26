7b4af47577aa9dc38f8a0e67b4700cbacc3d1e12
minhazapon <minhajulabedinapon@gmail.com>
2024-11-26 07:57:20 -0800

    added file

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -13,13 +13,13 @@ const Nav = () => {
         <div className=" navbar   " >     
         <div className=" navLogo nav-margin-left ">
         <NavLink to='/'>
-        <img className=" " src="https://cdn-icons-png.flaticon.com/128/5234/5234853.png" alt="" height="40px" width="50px" /> 
+        <img className=" res-icon " src="https://cdn-icons-png.flaticon.com/128/5234/5234853.png" alt="" height="40px" width="50px" /> 
         </NavLink>
         <p className="navName   ">FURNISH</p>
         </div>
         <div className=" navAllButton nav-margin-right " >
-        <NavLink to='/AboutMother'><button className=" navAbout font-dm-sans buttonHover">About Us</button></NavLink>
-        <NavLink to='/contact' ><button className=" navAbout font-dm-sans buttonHover">Contact Us</button></NavLink>
+        <NavLink to='/AboutMother'><button className="  navAbout font-dm-sans buttonHover">About Us</button></NavLink>
+        <NavLink to='/contact' ><button className="     navAbout font-dm-sans buttonHover">Contact Us</button></NavLink>
         <div className=" text-sign ">
         <button className=" navSign dm-sans  buttonHover ">Sign In</button>
         </div>
