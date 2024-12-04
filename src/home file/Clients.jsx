6a36bdd2c4cf7731eb04d31ac1f5adc3f19e1b7b
minhazapon <minhajulabedinapon@gmail.com>
2024-12-04 00:50:18 -0800

    added file

--- src/home file/Clients.jsx
+++ src/home file/Clients.jsx
@@ -1,5 +1,7 @@
 
 
+import r1 from '../pic file/r1.jpg'
+import r2 from '../pic file/r2.avif'
 
 const Clients = () => {
 
@@ -15,7 +17,7 @@ const Clients = () => {
             <div className=" all-client-card">
 
             <div className=" Client-cardOne ">
-            <img className=" img-border " height="270px" width="220px" src="https://s3-alpha-sig.figma.com/img/a15b/c589/63ffb301f03a98268a3c0ab584d30a79?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bU~M592OjHbAQl1p0Tl4RbOUNGdvVLW28rH3DeHaBUlTXqLQ-HlsC~SJyRDk285pIAXvm-p7MuMP6hRLGpkZaRxu6hAnvw-C81QaXAhEcIEyuXKLRpVFd80Ed7UE6ckzBZtYGZLFkyWsN2wh3AYhn1leXgLZFqWhVq5Y-3KY2DFgE5z9j~WCNU2OCSYXt1ZKkapu4R11sEBThKmnCctNjjaKzZ42SHScndF6XGkiCLYr2fqKc~LaU2pDmA1bkqXISrp~C4bdHa3VaBH9rR8hMEd8o5cVljaOl6CADY2BEdl87cJZeDqNcA5JOF9ktZvN9KBUhqsLGEH3z9TQw3Sp4A__"
+            <img className=" img-border " height="270px" width="220px" src={r1}
             alt="" />
             <div className=" C-R ">
             <img height="30px" src="https://cdn-icons-png.flaticon.com/128/17399/17399932.png" alt="" /> 
@@ -26,7 +28,7 @@ const Clients = () => {
             </div>
 
             <div className=" Client-cardOne ">
-            <img className=" img-border " height="270px" width="220px" src="https://img.freepik.com/free-photo/cheerful-bearded-man-busines-clothes-showing-thumb-up-looking-camera-gray_171337-11351.jpg?t=st=1731990396~exp=1731993996~hmac=9c2f09b78a621e93508bd9b9d4f316fb11ea3b694e89bf62c8d2744d1d991013&w=740"
+            <img className=" img-border " height="270px" width="220px" src={r2}
             alt="" />
             <div className=" C-R ">
             <img height="30px" src="https://cdn-icons-png.flaticon.com/128/17399/17399932.png" alt="" /> 
