7220df813006943813e1d5fca471dfec73c2338c
minhazapon <minhajulabedinapon@gmail.com>
2024-12-05 00:24:40 -0800

    added file

--- src/home file/PremiumRoom.jsx
+++ src/home file/PremiumRoom.jsx
@@ -4,7 +4,7 @@ const PremiumRoom = () => {
 
 
     return (
-        <div>
+        <div className=" allRoom ">
         <div className=" RoomPic ">
         <div className=" room-bg ">
         <p className=" RoomButton RoomText ">Display Package Premium <br></br>For Space Room</p>
